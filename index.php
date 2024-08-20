dc4f71b1bb5ac5f1637f9f97e1f0f53be10a8548
ahprinters <ahps323@gmail.com>
2024-08-20 20:34:44 +0600

    deleting echo

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello World";
+$hello = "Hello World";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo "I want to say" . $hello;?></h1>
+</body>
+</html>
